b9e71c77d9967f5f0cf155c67c204fe1feb12488
jhauck <jhauck@student.42heilbronn.de>
2026-06-24 02:03:43 +0200

    update var names to be more descriptive

--- src/ops_swap.c
+++ src/ops_swap.c
@@ -6,41 +6,41 @@
 /*   By: jhauck <jhauck@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/22 01:55:34 by jhauck            #+#    #+#             */
-/*   Updated: 2026/06/23 06:47:40 by jhauck           ###   ########.fr       */
+/*   Updated: 2026/06/24 02:02:46 by jhauck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-static void	swap_stack(t_stack *s)
+static void	swap_stack(t_stack *stack)
 {
 	int	tmp;
 
-	if (s->size < 2)
+	if (stack->size < 2)
 		return ;
-	tmp = s->top->value;
-	s->top->value = s->top->next->value;
-	s->top->next->value = tmp;
+	tmp = stack->top->value;
+	stack->top->value = stack->top->next->value;
+	stack->top->next->value = tmp;
 }
 
-void	op_sa(t_state *s)
+void	op_sa(t_state *state)
 {
-	swap_stack(s->a);
-	s->ops.sa++;
+	swap_stack(state->a);
+	state->ops.sa++;
 	write(1, "sa\n", 3);
 }
 
-void	op_sb(t_state *s)
+void	op_sb(t_state *state)
 {
-	swap_stack(s->b);
-	s->ops.sb++;
+	swap_stack(state->b);
+	state->ops.sb++;
 	write(1, "sb\n", 3);
 }
 
-void	op_ss(t_state *s)
+void	op_ss(t_state *state)
 {
-	swap_stack(s->a);
-	swap_stack(s->b);
-	s->ops.ss++;
+	swap_stack(state->a);
+	swap_stack(state->b);
+	state->ops.ss++;
 	write(1, "ss\n", 3);
 }
